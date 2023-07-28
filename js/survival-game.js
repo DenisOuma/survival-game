590a1d60124239905723e4e344e25d625f105ba1
Denis Ouma <dennisouma38@gmail.com>
2023-07-28 22:41:22 +0300

    Adding the players velocity movement around the X axis

---
+++ js/survival-game.js
@@ -0,0 +1,31 @@
+import MainScene from "./MainScene.js";
+
+const config = {
+	width: 900,
+	height: 500,
+	backgroundColor: "#333333",
+	type: Phaser.AUTO,
+	parent: "survival-game",
+	scene: [MainScene],
+	scale: {
+		zoom: 2,
+	},
+	physics: {
+		default: "matter",
+		matter: {
+			debug: true,
+			gravity: { y: 0 },
+		},
+	},
+	plugins: {
+		scene: [
+			{
+				plugin: PhaserMatterCollisionPlugin,
+				key: "matterCollision",
+				mapping: "matterCollision",
+			},
+		],
+	},
+};
+
+new Phaser.Game(config);
