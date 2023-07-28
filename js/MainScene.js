b2f614ed8e63a0b9cefcbe642a88cca78b195e09
Denis Ouma <dennisouma38@gmail.com>
2023-07-29 01:16:12 +0300

    Adding the static tile set to the website

--- js/MainScene.js
+++ js/MainScene.js
@@ -7,10 +7,21 @@ export default class MainScene extends Phaser.Scene {
 
 	preload() {
 		Player.preload(this);
+		this.load.image("tiles", "assets/imgs/RPG Nature Tileset.png");
+		this.load.tilemapTiledJSON("map", "assets/imgs/map.json");
 	}
 
 	create() {
-		console.log("create");
+		const map = this.make.tilemap({ key: "map" });
+		const tileset = map.addTilesetImage(
+			"RPG Nature Tileset",
+			"tiles",
+			32,
+			32,
+			0,
+			0
+		);
+		const layer1 = map.createLayer("Tile Layer 1", tileset, 0, 0);
 		this.player = new Player({
 			scene: this,
 			x: 0,
@@ -18,13 +29,7 @@ export default class MainScene extends Phaser.Scene {
 			texture: "male",
 			frame: "townsfolk_m_idle_1",
 		});
-		let textPlayer = new Player({
-			scene: this,
-			x: 100,
-			y: 100,
-			texture: "male",
-			frame: "townsfolk_m_idle_1",
-		});
+
 		this.player.inputKeys = this.input.keyboard.addKeys({
 			up: Phaser.Input.Keyboard.KeyCodes.W,
 			down: Phaser.Input.Keyboard.KeyCodes.S,
