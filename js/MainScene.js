6c251fbfeb8356aef32ddd99d58d9cdc0721c547
Denis Ouma <dennisouma38@gmail.com>
2023-07-29 00:43:20 +0300

    Setting up the application tile layer

--- js/MainScene.js
+++ js/MainScene.js
@@ -18,6 +18,13 @@ export default class MainScene extends Phaser.Scene {
 			texture: "male",
 			frame: "townsfolk_m_idle_1",
 		});
+		let textPlayer = new Player({
+			scene: this,
+			x: 100,
+			y: 100,
+			texture: "male",
+			frame: "townsfolk_m_idle_1",
+		});
 		this.player.inputKeys = this.input.keyboard.addKeys({
 			up: Phaser.Input.Keyboard.KeyCodes.W,
 			down: Phaser.Input.Keyboard.KeyCodes.S,
--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,24 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
 		let { scene, x, y, texture, frame } = data;
 		super(scene.matter.world, x, y, texture, frame);
 		this.scene.add.existing(this);
+
+		const { Body, Bodies } = Phaser.Physics.Matter.Matter;
+
+		var playerCollider = Bodies.circle(this.x, this.y, 12, {
+			isSensor: false,
+			lable: "playerCollider",
+		});
+		var playerSensor = Bodies.circle(this.x, this.y, 24, {
+			isSensor: true,
+			lable: "playerSensore",
+		});
+
+		const compoundBody = Body.create({
+			parts: [playerCollider, playerSensor],
+			frictionAir: 0.35,
+		});
+		this.setExistingBody(compoundBody);
+		this.setFixedRotation();
 	}
 
 	static preload(scene) {
@@ -14,8 +32,11 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
 		scene.load.animation("male_anim", "assets/imgs/male_anim.json");
 	}
 
+	get velocity() {
+		return this.body.velocity;
+	}
+
 	update() {
-		this.anims.play("male_idle", true);
 		const speed = 2.5;
 		let playerVelocity = new Phaser.Math.Vector2();
 		if (this.inputKeys.left.isDown) {
@@ -31,5 +52,10 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
 		playerVelocity.normalize();
 		playerVelocity.scale(speed);
 		this.setVelocity(playerVelocity.x, playerVelocity.y);
+		if (Math.abs(this.velocity.x) > 0.1 || Math.abs(this.velocity.y) > 0.1) {
+			this.anims.play("male_walk", true);
+		} else {
+			this.anims.play("male_idle", true);
+		}
 	}
 }
